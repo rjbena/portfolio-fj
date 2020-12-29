9b7c236425f6a6fcf781eecd0494c764e334a520
ricky <rjbenavides@cpp.edu>
2020-12-29 14:37:54 -0800

    create blog route

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,9 +4,41 @@ import Blog from "../models/blog.js";
 
 export const getBlogs = async (req, res) => {
   try {
-    const blogs = await Blog.find({});
-    res.json(blogs);
+    const blogs = await Blog.find({ status: "published" }).sort({
+      createdAt: -1,
+    });
+    return res.json(blogs);
   } catch (error) {
     res.status(422).send(error.message);
+  }
+};
+
+export const getBlogById = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    return res.json(blog);
+  } catch (error) {
+    return res.status(422).send(error.message);
+  }
+};
+
+export const getBlogBySlug = async (req, res) => {
+  try {
+    const blog = await Blog.findOne({ slug: req.params.slug });
+    return res.json(blog);
+  } catch (error) {
+    return res.status(422).send(error.message);
+  }
+};
+
+export const createBlog = async (req, res) => {
+  const newBlog = req.body;
+  newBlog.userId = "google-oauth2|104410789658557438999";
+  const blog = new Blog(newBlog);
+  try {
+    const createBlog = await blog.save();
+    return res.json(createBlog);
+  } catch (error) {
+    return res.status(422).send(error);
   }
 };
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,10 +1,20 @@
 import express from "express";
 
 import { checkJwt, checkRole } from "../middleware/auth.js";
-import { getBlogs } from "../controllers/blogs.js";
+import {
+  getBlogs,
+  getBlogById,
+  getBlogBySlug,
+  createBlog,
+} from "../controllers/blogs.js";
 
 const router = express.Router();
 
 router.get("/", getBlogs);
+router.post("/", createBlog);
+
+router.get("/:id", getBlogById);
+
+router.get("/s/:slug", getBlogBySlug);
 
 export default router;
