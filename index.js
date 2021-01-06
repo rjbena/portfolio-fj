fa596c1fe2334cba3bc9f850479a58f32f75937b
ricky <rjbenavides@cpp.edu>
2021-01-06 11:42:26 -0800

    fixed path error

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
-
+import path from "path";
 import dotenv from "dotenv";
 
 import portfoliosRoutes from "./routes/portfolios.js";
 import blogRoutes from "./routes/blogs.js";
 import connectDB from "./db/index.js";
+
+const __dirname = path.resolve(path.dirname(""));
 
 dotenv.config();
 const app = express();
@@ -15,8 +17,8 @@ async function runServer() {
   app.use("/api/v1/portfolios", portfoliosRoutes);
   app.use("/api/v1/blogs", blogRoutes);
 
-  app.get("", (req, res) => {
-    res.sendFile("index.html", { root: __dirname });
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
   });
 
   app.listen(PORT, (error) => {
